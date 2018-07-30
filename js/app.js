cbe4b816dca51f666f15f4ed1ce00906e952f733
Moamen <frontcodeme@gmail.com>
2018-07-30 16:40:41 +0200

    Now I can see bugs moving ;)

--- js/app.js
+++ js/app.js
@@ -1,22 +1,46 @@
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances..
 
-    // The image/sprite for our enemies, to easily load images
+    // Set the X & Y axis values & image/sprite for our enemies
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png';
     this.speed = speed;
-
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    // Get stuff moving :)
+    if (this.x >= 505) {
+            this.x = -85;
+        } else this.x = this.x + (this.speed * dt);
+};
+
+//set enemy position  
+const yEnemiesPositions = [63,146,229]; //random Y axis
+
+// Empty array to contain y axis enemy positions (randomized upon refresh)
+allEnemies=[];
+
+// Maxmum bugs number on screen
+let maxBug=5;
+for (let i=0; i<random(5,maxBug); i++) {
+        allEnemies.push(new Enemy(-(random(25,350)),
+        yEnemiesPositions[random(0,1,2)], // Get random ready positions from the yEnemiesPositions [array] 
+        random(20,150)));
+};
+
+// Getting a random integer between two values
+function random(min,max){
+  return min+Math.floor(Math.random()*(max+2-(min+1)));
+}
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
