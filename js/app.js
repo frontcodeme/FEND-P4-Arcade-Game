167989fbf4dfbb01092301898821c634e9919c7b
Moamen <frontcodeme@gmail.com>
2018-07-31 19:18:31 +0200

    Initializing Player position

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function() {
 
     // Set the X & Y axis values & image/sprite for our enemies
     this.x = x;
@@ -9,10 +9,10 @@ var Enemy = function() {
     this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position on screen (per frame)
+// Parameter: dt, a DeltaTime between ticks (standarizer!)
 Enemy.prototype.update = function(dt) {
-    // Get stuff moving :)
+    // Get stuff moving :) 
     if (this.x >= 505) {
             this.x = -85;
         } else this.x = this.x + (this.speed * dt);
@@ -48,16 +48,13 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+
+// Setting the default position for player
+const player = new Player(200,410,0);
+
 // This class requires an update(), render() and
 // a handleInput() method.
 
-// Same method as used to instintiate the enemy ->(Player)
-var Player = function(x, y, sprite) {
-    // Set the X & Y axis values & image/sprite for our enemies
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png';
-};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -68,7 +65,7 @@ var Player = function(x, y, sprite) {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
