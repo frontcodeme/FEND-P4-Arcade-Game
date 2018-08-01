2eb39a7a3686d73bd5b4ddbb93c19eb7c64ce1f5
Moamen <frontcodeme@gmail.com>
2018-08-02 00:07:20 +0200

    2D Collision Detection :D

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 
 // Enemies our player must avoid
-let Enemy = function() {
+let Enemy = function(x, y, speed) {
 
     // Set the X & Y axis values & image/sprite for our enemies
     this.x = x;
     this.y = y;
+    this.width = 70;
+    this.height = 50;
     this.sprite = 'images/enemy-bug.png';
     this.speed = speed;
 };
@@ -37,30 +39,80 @@ function random(min,max){
   return min+Math.floor(Math.random()*(max+2-(min+1)));
 }
 
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
+var Player = function(x, y, sprite) {
 
-// Setting the default position for player
-const player = new Player(200,410,0);
+    this.x = x;
+    this.y = y;
+    this.width = 70;
+    this.height = 50;
+    this.sprite = 'images/char-boy.png';
+};
 
-// This class requires an update(), render() and
-// a handleInput() method.
 
+//2D collision detiction MDN
+Player.prototype.checkCollisions = function () {
+    for (let i=0; i<allEnemies.length; i++) {
+      let  enemyChecked = allEnemies[i];
+        if (this.x<enemyChecked.x+enemyChecked.width
+            && this.x+this.width>enemyChecked.x
+            && this.y<enemyChecked.y+enemyChecked.height
+            && this.y+this.height>enemyChecked.y)
+          {
+            this.x = 200;
+            this.y = 415;
+        };
+    };
+}
+
+// update()
+Player.prototype.update = function() {
+    //call collision detection
+    this.checkCollisions();
+};
+
+
+//TODO:  render()
+
+Player.prototype.reset = function() {
+
+    this.x = 200;
+    this.y = 415;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+};
+
+// a handleInput() method on keystroke event
+Player.prototype.handleInput = function(move) {
+
+    if (move === 'right' && this.x < 400) {
+        this.x += 100;
+    }
+    if (move === 'left' && this.x > 0) {
+        this.x -= 100;
+    }
+    if (move === 'up' && this.y >= 20) {
+        this.y -= 85;
+    }
+    if (move === 'down' && this.y <= 350) {
+        this.y += 85;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+// Setting the default position for player
+const player = new Player(200,410,0);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -74,3 +126,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+//TODO: DEBUG needs to reset player position upon reaching rever!
