82ff6985ec750f88413d63f497127ba82bd0167a
Moamen <frontcodeme@gmail.com>
2018-08-02 23:26:20 +0200

    Finally the game logic is logical

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let Enemy = function(x, y, speed) {
     // Set the X & Y axis values & image/sprite for our enemies
     this.x = x;
     this.y = y;
+    // width & hight properties are helping the collision detection
     this.width = 70;
     this.height = 50;
     this.sprite = 'images/enemy-bug.png';
@@ -20,18 +21,23 @@ Enemy.prototype.update = function(dt) {
         } else this.x = this.x + (this.speed * dt);
 };
 
-//set enemy position  
-const yEnemiesPositions = [63,146,229]; //random Y axis
+// set enemy position  
+const yEnemiesPositions = [63,146,229]; //alligned Y axis positions
 
-// Empty array to contain y axis enemy positions (randomized upon refresh)
+// Empty array to contain bugs (always changing)
 allEnemies=[];
 
 // Maxmum bugs number on screen
 let maxBug=5;
+
+// increament enemeys continuasly to be at leat five on the screen 
 for (let i=0; i<random(5,maxBug); i++) {
+        // push random position for enemy each time
         allEnemies.push(new Enemy(-(random(25,350)),
-        yEnemiesPositions[random(0,1,2)], // Get random ready positions from the yEnemiesPositions [array] 
-        random(20,150)));
+        // ready positions from the yEnemiesPositions [array] 
+        yEnemiesPositions[random(0,1,2)], 
+        // speed
+        random(50,150)));
 };
 
 // Getting a random integer between two values
@@ -44,25 +50,26 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// player function that contains all the player properties
 var Player = function(x, y, sprite) {
 
     this.x = x;
     this.y = y;
+    // width & hight properties are helping the collision detection
     this.width = 70;
     this.height = 50;
     this.sprite = 'images/char-boy.png';
 };
 
 
-//2D collision detiction MDN
+// 2D collision detiction MDN (was so hard to figure out!)
 Player.prototype.checkCollisions = function () {
     for (let i=0; i<allEnemies.length; i++) {
-      let  enemyChecked = allEnemies[i];
-        if (this.x<enemyChecked.x+enemyChecked.width
-            && this.x+this.width>enemyChecked.x
-            && this.y<enemyChecked.y+enemyChecked.height
-            && this.y+this.height>enemyChecked.y)
+      let  position = allEnemies[i];
+        if (this.x<position.x+position.width
+            && this.x+this.width>position.x
+            && this.y<position.y+position.height
+            && this.y+this.height>position.y)
           {
             this.x = 200;
             this.y = 415;
@@ -70,61 +77,52 @@ Player.prototype.checkCollisions = function () {
     };
 }
 
-// update()
+// always check for player condition 
+// to minipulate position & status accordingly   
 Player.prototype.update = function() {
-    //call collision detection
+    // call collision detection
     this.checkCollisions();
+    // check if player has reached the water :D
+    if (this.y <= 20) {
+        console.log("can you swim? ;)");
+        // put player back to starting position
+        this.reset();
+    }
 };
 
-
-//TODO:  render()
-
+// reset player position (upon colission or win)
 Player.prototype.reset = function() {
-
+    // default position on the canva
     this.x = 200;
     this.y = 415;
 };
 
+// draw player image from its source to display on canva
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
 };
 
-// a handleInput() method on keystroke event
+// a handleInput() method for keystroke event:
 Player.prototype.handleInput = function(move) {
-
+// listen to keys & "change player position accordingly"
     if (move === 'right' && this.x < 400) {
         this.x += 100;
-    }
-    if (move === 'left' && this.x > 0) {
+    } if (move === 'left' && this.x > 0) {
         this.x -= 100;
-    }
-    if (move === 'up' && this.y >= 20) {
+    } if (move === 'up' && this.y >= 20) {
         this.y -= 85;
-    }
-    if (move === 'down' && this.y <= 350) {
+    } if (move === 'down' && this.y <= 350) {
         this.y += 85;
     }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Setting the default (Starting) position for player on the canva
+const player = new Player(200,415,0);
 
-// Setting the default position for player
-const player = new Player(200,410,0);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// listen for keystroke & sends the keys to Player.handleInput()
 document.addEventListener('keyup', function(e) {
     let allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: 'left', 38: 'up', 39: 'right', 40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-//TODO: DEBUG needs to reset player position upon reaching rever!
